972b2a018839579328e1d2794bb27032fa606519
Alex Mendoza <amendoza807@gmail.com>
2023-10-01 17:32:18 -0500

    fix message

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-// Animación del mensaje
-const message = document.getElementById("message1");
+const next = document.getElementById("next");
 setTimeout(() => {
-  message1.style.opacity = "1";
-}, 3000); // Mostrar el mensaje después de 3 segundos
-const message2 = document.getElementById("message2");
-setTimeout(() => {
-  message2.style.opacity = "1";
-}, 5000); // Mostrar el mensaje después de 3 segundos
+  next.style.opacity = "1";
+}, 2000);
+function iniciarDespuesDeCerrarModal() {
+  // Animación del mensaje 1
+  const message1 = document.getElementById("message1");
+  setTimeout(() => {
+    message1.style.opacity = "1";
+  }, 1000); // Mostrar el mensaje después de 0 segundos
+  const img = document.getElementById("img");
+  setTimeout(() => {
+    img.style.opacity = "1";
+  }, 4000); // Mostrar el mensaje después de 0 segundos
 
+  // Animación del mensaje 2
+  const message2 = document.getElementById("message2");
+  setTimeout(() => {
+    message2.style.opacity = "1";
+  }, 6000); // Mostrar el mensaje después de 2 segundos
+
+  // Reproducir música
+  const cancion = document.getElementById("cancion");
+  cancion.style.display = "none"; // Oculta los controles de reproducción
+  cancion.play(); // Inicia la reproducción automáticamente
+}
+
+// Mostrar el modal al cargar la página
+const modal = document.getElementById("myModal");
+window.addEventListener("load", () => {
+  modal.style.display = "block";
+});
+
+// Reproducir música y comenzar animaciones después de hacer clic en cualquier lugar de la ventana
 window.addEventListener("click", () => {
-  cancion.style.display = "none"; // Muestra los controles de reproducción
-  cancion.play();
+  modal.style.display = "none"; // Cierra el modal
+  iniciarDespuesDeCerrarModal(); // Llama a la función para iniciar todo
 });
