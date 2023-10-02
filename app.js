9a29340273d8e6ee7f4515dccc62b8f159bbfd4a
Alex Mendoza <amendoza807@gmail.com>
2023-10-01 20:02:55 -0500

    better

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ function iniciarDespuesDeCerrarModal() {
     message2.style.opacity = "1";
   }, 6000); // Mostrar el mensaje después de 2 segundos
 
+  const openModalButton = document.getElementById("openImage");
+  setTimeout(() => {
+    openModalButton.style.opacity = "1";
+  }, 9000);
+
   // Reproducir música
   const cancion = document.getElementById("cancion");
   cancion.style.display = "none"; // Oculta los controles de reproducción
@@ -35,4 +40,16 @@ window.addEventListener("load", () => {
 window.addEventListener("click", () => {
   modal.style.display = "none"; // Cierra el modal
   iniciarDespuesDeCerrarModal(); // Llama a la función para iniciar todo
+});
+
+const frio = document.getElementById("frio");
+const openImage = document.getElementById("openImage");
+
+openImage.addEventListener("click", () => {
+  frio.style.display = "block";
+});
+
+frio.addEventListener("click", () => {
+  // Corrección aquí
+  frio.style.display = "none";
 });
